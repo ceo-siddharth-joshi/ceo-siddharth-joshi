ea8a07cc2153cf7c939eb010df496367f831f367
ceo-siddharth-joshi <141755658+ceo-siddharth-joshi@users.noreply.github.com>
2023-08-26 21:40:57 +0530

    Add files via upload

---
+++ main/_next/static/qIfS1wSRnwkzgJ2WzS_sU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b3fbaaec264569ee.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-5fb8df814d4d84e1.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
